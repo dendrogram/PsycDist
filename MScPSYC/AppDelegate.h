ea368a0b9d83014e064ac61cb8524b840f8482d7
Jonathan Howell <jonathanhowell@imacjonno21.lan>
2013-10-19 22:34:50 +0100

    Initial Commit

---
+++ MScPSYC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MScPSYC
+//
+//  Created by Jonathan Howell on 19/10/2013.
+//  Copyright (c) 2013 Jonathan Howell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
